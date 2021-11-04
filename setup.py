6cc0ee9255bba90974c231c0e090d9cf67a66401
alvin-c-cruz <alvinccruz12@gmail.com>
2021-11-04 20:56:30 +0700

    Added configuration files.

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
 
 setup(
     name='data_model',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+    packages = ['data_model'],
+    version = '0.1',
+    license = 'mit',
+    description='A class using the built-in @dataclass decorator in conjunction with sqlite3.',
+    author='Alvin C. Cruz',
+    author_email='alvinccruz12@gmail.com',
+    url='https://github.com/alvin-c-cruz/data_model',
+    download_url='https://github.com/alvin-c-cruz/data_model/archive/refs/tags/v_01.tar.gz',
+    keywords=['dataclass'],
     install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3.7',      
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        ],
 )
