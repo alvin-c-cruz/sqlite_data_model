5c27da117c1cb5d31b097de2f38b5a7049f9db47
alvin-c-cruz <alvinccruz12@gmail.com>
2021-11-06 05:17:48 +0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='sqlite_data_model',
     packages = ['sqlite_data_model'],
-    version = '0.1',
+    version = '0.2',
     license = 'mit',
     description='A class using the built-in @dataclass decorator in conjunction with sqlite3.',
     author='Alvin C. Cruz',
     author_email='alvinccruz12@gmail.com',
     url='https://github.com/alvin-c-cruz/sqlite_data_model',
-    download_url='https://github.com/alvin-c-cruz/sqlite_data_model/archive/refs/tags/v_01.tar.gz',
-    keywords=['dataclass'],
+    download_url='https://github.com/alvin-c-cruz/sqlite_data_model/archive/refs/tags/v0.2.tar.gz',
+    keywords=['dataclass', 'sqlite', 'datamodel'],
     install_requires=[],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
